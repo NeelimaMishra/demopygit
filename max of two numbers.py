eeee5751fa587b2a2de037c32c2f5241d8a8a41b
NeelimaMishra <r.neelimamishra@gmail.com>
2019-09-10 18:14:40 +0530

    Write a function that returns the maximum of two numbers.

---
+++ max of two numbers.py
@@ -0,0 +1,11 @@
+# Write a function that returns the maximum of two numbers.
+def max(a, b):
+
+    if a > b:
+        return a
+    else:
+        return b
+
+
+res = max(10,1)
+print('Max of two numbers is: ', res)
