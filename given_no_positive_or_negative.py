d0783c22b18f6ef7fd0a9ae04693f29c9bc17ee3
NeelimaMishra <r.neelimamishra@gmail.com>
2019-09-22 13:10:19 +0530

    write a code to check the given number is negative or positive

---
+++ given_no_positive_or_negative.py
@@ -0,0 +1,7 @@
+#write a code to check the given number is negative or positive
+
+n = int(input('Enter an integer number: '))
+if n >= 0:
+    print('Number is positive number')
+else:
+    print('Number is negitive number')
