8b6a44ef9d7f3e1ec6b37c4ad899384998ed8288
Neelima Mishra <r.neelimamishra@gmail.com>
2020-06-17 23:22:32 +0530

    Update add_even_odd_index_values_of_list.py

--- add_even_odd_index_values_of_list.py
+++ add_even_odd_index_values_of_list.py
@@ -2,7 +2,6 @@
 
 l1 = [1, 100, 3, 4, 5, 6]
 
-'''
 #One way of doing this program is following
 evenl1 = l1[::2]
 print("Even index list is: ", evenl1)
@@ -16,8 +15,8 @@ sumodd = 0
 for i in oddl1:
     sumodd += i
 print(sumodd)
-'''
 
+#Second way
 sum_even = 0
 sum_odd = 0
 flag = True
@@ -31,3 +30,13 @@ for i in l1:
 print("sum of even index in l1 is: ",sum_even)
 print("sum of odd index in l1 is: ", sum_odd)
 
+#Third way
+l1 = [1,33,6,9,4]
+sum_even_no_l1 = 0
+sum_odd_no_l1 = 0
+for i in range(0, len(l1), 2):
+  sum_even_no_l1 = sum_even_no_l1 + l1[i]
+for i in range(1, len(l1), 2):
+  sum_odd_no_l1 = sum_odd_no_l1 + l1[i]
+print(sum_even_no_l1)
+print(sum_odd_no_l1)
