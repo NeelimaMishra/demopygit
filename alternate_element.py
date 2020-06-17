2c54917dbf130c1f6eee5a3d93fd95c0259c530a
Neelima Mishra <r.neelimamishra@gmail.com>
2020-06-17 22:56:30 +0530

    Update alternate_element.py

--- alternate_element.py
+++ alternate_element.py
@@ -1,7 +1,7 @@
 #Alternate element in list
 
 l1 = [5, 10, 15, 0, 7, 9, 11]
-'''
+
 # one of the way of doing this program
 new1_l1 = []
 new2_l1 = []
@@ -15,10 +15,21 @@ for i in l1:
         flag = True
 print(new1_l1)
 print(new2_l1)
-'''
+
+#second approach
 new_l1 = []
 count = 0
 while count < len(l1):
     new_l1.append(l1[count])
     count += 2
 print(new_l1)
+
+#third approach
+odd_new_l1 = []
+even_new_l1 = []
+for i in range(0, len(l1), 2):
+  odd_new_l1.append(l1[i])
+for i in range(1, len(l1), 2):
+  even_new_l1.append(l1[i])
+print(even_new_l1)
+print(odd_new_l1)
